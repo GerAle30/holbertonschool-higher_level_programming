4eb26740e410eeb38ff4f98d4215a31dc635bd63
GerAle30 <garciaslj@gmail.com>
2025-05-11 22:31:12 -0400

    mensaje

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "Python"
-# Concatenate to form the required output
-print(str1 + " programming with " + str2)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# Extract the required parts using slicing
+print(str[39:54] + " " + str[55:66] + " " + str[67:71] + " " + str[:6])
