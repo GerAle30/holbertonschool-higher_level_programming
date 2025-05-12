4f3974b770cdfa86d080721049a711305e37e44e
GerAle30 <garciaslj@gmail.com>
2025-05-11 22:17:19 -0400

    mensaje

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
 str2 = "Holberton School"
-str1 = str1 + " " + str2 + "!"
-                     print(str1)
+# Print the concatenated strings with the exclamation mark
+print(str1 + " " + str2 + "!")
