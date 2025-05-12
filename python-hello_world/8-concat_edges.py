70ca35a4bb292e8e77f22631fefa2f69fdb85381
GerAle30 <garciaslj@gmail.com>
2025-05-11 22:32:01 -0400

    mensaje

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# Extract the required parts using slicing
-print(str[39:54] + " " + str[55:66] + " " + str[67:71] + " " + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming " \
+      "language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
