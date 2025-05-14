0b6bc9f543227ba2e647339370f4eeb6e2b17212
GerAle30 <garciaslj@gmail.com>
2025-05-14 12:52:25 -0400

    mensaje

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number + rnaodm.randint(-10, 10)
+
+if number > 0:
+    print(f"{number} is positive")
+elif numer == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
