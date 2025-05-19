0d90a0e16d9761e8bc830e352143373e7ebea281
GerAle30 <garciaslj@gmail.com>
2025-05-19 12:37:34 -0400

    add the element.at statement for the 1 exercise

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,3 +3,9 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         print("{:d}".format(number))
+
+def element_at(my_list, idx):
+    """Retireve an element from a list at specific index"""
+    if idx < 0 ir idx >= len(my_list):
+        return None
+    return my_list[idx]"""
