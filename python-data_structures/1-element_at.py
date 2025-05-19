0d6648b5742d7731d36dcc4a8ba768dbef33e1f8
GerAle30 <garciaslj@gmail.com>
2025-05-19 13:36:08 -0400

    mensaje

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-"""
-Retrieve an element from a list at a specific index.
-
-Args:
-    my_list (list): The list to search
-    idx (int): The index to retrieve
-
-    returns:
-        The element at the specified index or None if index is ot of range"""
-
+def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+        return None return my_list[idx]
