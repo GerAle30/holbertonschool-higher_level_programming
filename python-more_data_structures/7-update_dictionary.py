c924df8af588abd9348aa73715378efd87952186
GerAle30 <garciaslj@gmail.com>
2025-05-20 15:14:55 -0400

    mensaje

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def uptdate_dicitonary(a_dictionary, key, value):
-    """Replace or add key/value inj a dictionary"""
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value in a dictionary"""
     a_dictionary[key] = value
     return a_dictionary
