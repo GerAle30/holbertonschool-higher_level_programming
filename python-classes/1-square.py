8e9cfb1080236dd9f244f9f05955770fd3d3c656
GerAle30 <garciaslj@gmail.com>
2025-05-24 19:32:08 -0400

    Refactor Square class definition for consistency and clarity

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class Square with a private instance
-attribute size."""
-class square:
+"""Module that defines a class Square"""
+
+
+class Square:
     """Represents a square."""
     def __init__(self, size):
-        """
-        Initializes the square with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
