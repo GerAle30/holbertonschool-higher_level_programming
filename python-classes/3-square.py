ed80f95044f1e0eb78c6f0755f6208adee640920
GerAle30 <garciaslj@gmail.com>
2025-05-24 19:55:11 -0400

    mensaje

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Square with area method."""
-
+"""Defines a class Square with size validation and area method."""
 
 
 class Square:
     """Represents a square."""
+
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initializes a square.
 
         Args:
-            size (int): The size of the new square.
-        
+            size (int): Size of the square (default 0).
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
@@ -18,14 +18,9 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-
             raise ValueError("size must be >= 0")
         self.__size = size
-        def area(self):
-            """Calculate and return the area of square.
-            
-            Returns:
-            The area of the square.
-            """
-            return self.__size ** 2
-        
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
