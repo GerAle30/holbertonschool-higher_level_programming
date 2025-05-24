22e3d1c645f700d20d6b7f5c7787cfb45c1666db
GerAle30 <garciaslj@gmail.com>
2025-05-24 19:50:52 -0400

    Fix formatting by adding newlines and removing unnecessary blank lines in the Square class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Defines a class Square with area method."""
+
+
+
 class Square:
     """Represents a square."""
     def __init__(self, size=0):
@@ -15,6 +18,7 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
+
             raise ValueError("size must be >= 0")
         self.__size = size
         def area(self):
@@ -24,3 +28,4 @@ class Square:
             The area of the square.
             """
             return self.__size ** 2
+        
