d917d36c91e087dcac94cede32a07a9dc93f1b68
GerAle30 <garciaslj@gmail.com>
2025-05-28 14:24:07 -0400

    Fix formatting by adding a newline at the end of the function docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """This module defines a function to look up the methods"""
 def lookup(obj):
+    """ 
+    Returns a list of attributes adn methods of an object
     
+    Args:
+    obj: The object to inspect.
+    
+    Returns:
+    A list containing the names of the object's attribute and methods."""
     return dir(obj)
