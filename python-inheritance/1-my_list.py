46c9b19b145995b2f4f729539728ff08b24d38fe
GerAle30 <garciaslj@gmail.com>
2025-05-30 12:10:06 -0400

    Fix formatting and typos in docstrings for MyList class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines a class MyList that inherits from list."""
+""" This module defines a class MyList that inherits from list."""
 
 class Mylist(list):
-    """
-    Custom list class that adds a print_sorted method."""
+    """Custom list class that adds a print_sorted method."""
     
     def print_dorted(self):
-        """
-        Prints the list insorted (ascending)
+        """ Prints the list insorted (ascending)
         order without modyifying the original
         list"""
         print(sorted(self))
