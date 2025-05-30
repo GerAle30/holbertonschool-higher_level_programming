27492b12d0ea70331fdb97965b00feea71ed3621
GerAle30 <garciaslj@gmail.com>
2025-05-30 13:26:21 -0400

    mensaje

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Function to check if an object is exactly an instance of a class."""
+"""Defines a function that checks for object-class identity."""
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of the specified class.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-
-    Returns:
-        bool: True if obj is exactly an instance of a_class, False otherwise.
+    """
+    Return True if the object is exactly an instance of the specified class.
+    Otherwise, return False.
     """
     return type(obj) is a_class
