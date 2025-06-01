5a6e3798442ac21dc35929d2e8f8ac1acab6f837
GerAle30 <garciaslj@gmail.com>
2025-06-01 12:24:18 -0400

    mensaje

---
+++ python-abc/task-00_abc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines an abstract Animal class and two subclasses Dog and Cat."""
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Abstract base class for animals."""
+
+    @abstractmethod
+    def sound(Self):
+        """Abstract method that should be implemented by subclasses."""
+        pass
+
+    class Dog(Animal):
+        """Dog class that implements the Animal interface."""
+
+        def sound(self):
+            """Return the sound of a dog."""
+            return "Bark"
+
+        class Cat(Animal):
+            """Cat class that implements the Animal interface."""
+
+            def sound(self):
+                """Return the sound of a cat."""
+                return "Meow"
