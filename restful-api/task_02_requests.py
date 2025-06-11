574abf208fca75464965dbc399e68dfe5ca908df
GerAle30 <garciaslj@gmail.com>
2025-06-11 16:36:08 -0400

    mensaje

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,26 +7,26 @@ def fetch_and_print_posts():
     response = requests.get(url)
 
     print(f"Status Code: {response.status_code}")
-
-    if rerponse.status_code == 200:
+    
+    if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post["title"])
 
-            def fetch_and_save_posts():
-                """Fetches posts and saves them into a csv file"""
-                url = "https://jsonplaceholder.typicode.com/posts"
-                response = requests.get(url)
+def fetch_and_save_posts():
+    """Fetches posts and saves them into a CSV file"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
 
-                if response.status_code == 200:
-                    posts = response.json()
-                    data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+    if response.status_code == 200:
+        posts = response.json()
+        data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
 
-                    with open("posts.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-                        fieldnames = ["id", "title", "body"]
-                         write = csv.DictWrite(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for post in data:
+                writer.writerow(post)
 
-                         write.writeheade()
-                         for post in data:
-                             writer.writerow(post)
