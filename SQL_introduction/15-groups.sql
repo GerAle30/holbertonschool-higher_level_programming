1957d3ba6a8c8ce78b96db9cc08e3c02235141d8
GerAle30 <garciaslj@gmail.com>
2025-06-28 08:17:28 -0400

    fix: correct syntax error in SQL query for grouping and ordering records

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists the number of records for each scroe in second in second_table
 SELECT score, COUNT (*) AS number
 FROM second_table
-GROUP BY score <order by number DESC;
+GROUP BY score ORDER BY number DESC;
