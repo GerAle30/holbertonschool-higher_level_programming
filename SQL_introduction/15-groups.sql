648a64c92f4c2670282a332346c41b1e016e6cc0
GerAle30 <garciaslj@gmail.com>
2025-06-28 08:33:58 -0400

    fix: correct spacing in SQL query for counting records in second_table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- Lists the number of records for each scroe in second in second_table
-SELECT score, COUNT (*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score ORDER BY number DESC;
+GROUP BY score 
+ORDER BY number DESC;
