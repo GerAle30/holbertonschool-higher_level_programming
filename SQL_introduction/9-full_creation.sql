06b71b2e96c5affeb036913e080c942ab37d6556
GerAle30 <garciaslj@gmail.com>
2025-06-28 08:03:29 -0400

    fix: add space after comma in SQL insert statement for consistency

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -8,7 +8,7 @@ CREATE TABLE_IF NOT EXISTS second_table (
 
 -- Insert 4 records into second _table
 INSERT INTO second_table (id, name, score) VALUES
-(1, 'John',10),
+(1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
