1aaced52c121c519203eb41794f3658f9cf5663e
GerAle30 <garciaslj@gmail.com>
2025-07-27 23:00:13 -0400

    add,Square,LOve C file do it correctly

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that computes and prints a factorial
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
